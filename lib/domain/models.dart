5cc7b71056badfd2ccde0c3965671ac35a12e561
nourhanalturk <121407217+nourhanalturk@users.noreply.github.com>
2023-09-02 01:52:18 +0300

    Adding login models

--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -13,3 +13,28 @@ class SliderViewObject { // data that we will pass from view model to the view
   int currentIndex ;
   SliderViewObject(this.sliderObject,this.numOfSlides,this.currentIndex);
 }
+
+//login models
+class Customer {
+  String id;
+  String name;
+  int numOfNotifications;
+
+  Customer(this.id, this.name, this.numOfNotifications);
+}
+
+class Contacts {
+  String phone;
+  String email;
+  String link;
+
+  Contacts(this.phone, this.email, this.link);
+}
+
+class Authentication {
+  Customer? customer;
+  Contacts? contacts;
+  // accept null because i don't need to create an empty value to obj we can do it easily with primitive types
+
+  Authentication(this.customer, this.contacts);
+}
