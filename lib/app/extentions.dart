2a2aabdea47bd0e7900f2b4a9d326ef8b269de43
nourhanalturk <121407217+nourhanalturk@users.noreply.github.com>
2023-09-02 01:51:27 +0300

    Adding String and int extentions

---
+++ lib/app/extentions.dart
@@ -0,0 +1,23 @@
+// response from data layer to domain layer from null values to non nullable value
+import 'constants.dart';
+
+extension NonNullString on String?{
+
+  String orEmpty (){
+    if(this ==null){
+      return Constants.empty;
+    }else{
+      return this! ;
+    }
+  }
+}
+extension NonNullInteger on int?{
+
+  int orZero (){
+    if(this ==null){
+      return Constants.zero;
+    }else{
+      return this! ;
+    }
+  }
+}
